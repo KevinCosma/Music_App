9abeec219c61162306a8f212fe604cf470fe7879
KevinCosma <kcosman2005@gmail.com>
2021-10-08 11:38:46 -0500

    added getAllSongs, getSpecificSong, addNewSong, updateSong, and deleteSong code functions, but without the axios. Need to make different components and then add axios links.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,61 @@ class App extends Component {
     super(props);
     this.state = {  }
   }
+
+  componentDidMount() {
+    this.getAllSongs();
+    this.getSpecificSong();
+    this.addNewSong();
+    this.updateSong();
+    this.deleteSong();
+  }
+
+  async getAllSongs() {
+    try{
+      let response = await axios.get();
+      console.log(response.data)
+    }
+    catch (ex) {
+      console.log('Error in API call!');
+    }
+  }
+
+  async getSpecificSong() {
+    try{
+      let response = await axios.get();
+    }
+    catch (ex) {
+      console.log('Error in API call!');
+    }
+  }
+
+  async addNewSong() {
+    try{
+      let response = await axios.get();
+    }
+    catch (ex) {
+      console.log('Error in API call');
+    }
+  }
+
+  async updateSong() {
+    try{
+      let response = await axios.get()
+    }
+    catch (ex) {
+      console.log('Error in API call!');
+    }
+  }
+
+  async deleteSong() {
+    try{
+      let response = await axios.get()
+    }
+    catch (ex) {
+      console.log('Error in API call!');
+    }
+  }
+
   render() { 
     return ( 
       <h1>Hello World</h1>
