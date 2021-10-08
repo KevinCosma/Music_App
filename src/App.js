76da73fe5bbbffcb6213756d4b25871b0cf36d93
KevinCosma <kcosman2005@gmail.com>
2021-10-08 10:50:35 -0500

    re-did the npx and npm installs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
-
-
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {  }
   }
   render() { 
     return ( 
-      <div>
-          <h1>Hello World</h1>
-      </div>
+      <h1>Hello World</h1>
      );
   }
 }
